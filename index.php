6bde245c0db3386713c2f7110ed66e1815f1c88a
Anowar Hossain Apurbo <ahosien57@gmail.com>
2021-09-24 01:53:50 +0600

    final commit

--- index.php
+++ index.php
@@ -17,38 +17,112 @@ session_start();
 
   <?php 
 
-    if(isset($_POST['count'])){
-            if(empty($_SESSION['count'])){
-                $_SESSION['count'] = 1;
+    // Subhan Allah function start
+    if(isset($_POST['subhanallah'])){
+            if(empty($_SESSION['subhanallah'])){
+                $_SESSION['subhanallah'] = 1;
 
             }else{
-                $count = $_SESSION['count'] + 1;
-                $_SESSION['count'] = $count;
+                $subhanallah = $_SESSION['subhanallah'] + 1;
+                $_SESSION['subhanallah'] = $subhanallah;
             }
         }
-
-        if(isset($_POST['reset'])){
-            session_destroy();
+        if(isset($_POST['reset_subhan'])){
+            $_SESSION['subhanallah'] = 0;
             header("location: index.php");
         }
+    // Subhan Allah function end
+
+
+    // alhamdulillah function start
+    if(isset($_POST['alhamdulillah'])){
+            if(empty($_SESSION['alhamdulillah'])){
+                $_SESSION['alhamdulillah'] = 1;
+
+            }else{
+                $alhamdulillah = $_SESSION['alhamdulillah'] + 1;
+                $_SESSION['alhamdulillah'] = $alhamdulillah;
+            }
+        }
+        if(isset($_POST['reset_alham'])){
+            $_SESSION['alhamdulillah'] = 0;
+            header("location: index.php");
+        }
+    // alhamdulillah function end
+
+
+    // allahu akbar function start
+    if(isset($_POST['allahuakbar'])){
+            if(empty($_SESSION['allahuakbar'])){
+                $_SESSION['allahuakbar'] = 1;
+
+            }else{
+                $allahuakbar = $_SESSION['allahuakbar'] + 1;
+                $_SESSION['allahuakbar'] = $allahuakbar;
+            }
+        }
+        if(isset($_POST['reset_allahu'])){
+            $_SESSION['allahuakbar'] = 0;
+            header("location: index.php");
+        }
+    // allahu akbar function end
         
-    
    ?>
 
   <div class="main">
     <div class="twarp">
       
-   <div class="display"> <span id="tnum" ><?php if(empty($_SESSION['count'])){
+   <div class="display"> <span id="tnum" ><?php if(empty($_SESSION['subhanallah'])){
     echo '0';
   }else{
-    echo $_SESSION['count'];
+    echo $_SESSION['subhanallah'];
   } ?></span></div>
 
 
  <div class="btnwrap">
       <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
-          <input class="tbtn" type="submit" value="Count" name="count">
-          <input class="rbtn" type="submit" value="Reset" name="reset">
+          <input class="tbtn" type="submit" value="Subhan Allah" name="subhanallah">
+          <input class="rbtn" type="submit" value="Reset" name="reset_subhan">
+      </form>
+</div>
+
+    </div>
+</div>
+
+<div class="main">
+    <div class="twarp">
+      
+   <div class="display"> <span id="tnum" ><?php if(empty($_SESSION['alhamdulillah'])){
+    echo '0';
+  }else{
+    echo $_SESSION['alhamdulillah'];
+  } ?></span></div>
+
+
+ <div class="btnwrap">
+      <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+          <input class="tbtn" type="submit" value="Alhamdulillah" name="alhamdulillah">
+          <input class="rbtn" type="submit" value="Reset" name="reset_alham">
+      </form>
+</div>
+
+    </div>
+</div>
+
+<div class="main">
+    <div class="twarp">
+      
+   <div class="display"> <span id="tnum" ><?php if(empty($_SESSION['allahuakbar'])){
+    echo '0';
+  }else{
+    echo $_SESSION['allahuakbar'];
+  } ?></span></div>
+
+
+ <div class="btnwrap">
+      <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+          <input class="tbtn" type="submit" value="Allahu Akbar" name="allahuakbar">
+          <input class="rbtn" type="submit" value="Reset" name="reset_allahu">
       </form>
 </div>
 
